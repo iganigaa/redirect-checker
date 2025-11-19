0bbb9b985a51619092aa0d37d473620fd61db16e
igor <igorr.burdukov@gmail.com>
2025-11-19 23:59:56 +0800

    Add Yandex Metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105412966', 'ym');
+
+            ym(105412966, 'init', {
+              ssr:true,
+              webvisor:true,
+              clickmap:true,
+              ecommerce:"dataLayer",
+              accurateTrackBounce:true,
+              trackLinks:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/105412966" 
+              style={{ position: 'absolute', left: '-9999px' }} 
+              alt="" 
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div className="flex min-h-screen bg-gray-50">
           {/* Sidebar */}
